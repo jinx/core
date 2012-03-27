fa28eee6f4b1698429687a5eb7feed4548ecae5d
Fred Loney <loneyf@ohsu.edu>
2012-03-27 14:19:49 -0700

    Move to_boolean to Boolean.for.

--- lib/jinx/helpers/boolean.rb
+++ lib/jinx/helpers/boolean.rb
@@ -1,46 +1,42 @@
-# Boolean marks the +true+ and +false+ primitive objects.
-module Boolean; end
+module Jinx
+  # Boolean marks the +true+ and +false+ primitive objects.
+  module Boolean
+    # The match for a +true+ String.
+    TRUE_REGEXP = /^(t(rue)?|y(es)?|1)$/i
+  
+    # The match for a +false+ String.
+    FALSE_REGEXP = /^(f(alse)?||no?|0)$/i
+  
+    # Converts the given object to a Boolean as follows:
+    # * If the object is nil or Boolean, then the unconverted value
+    # * 1 -> true
+    # * 0 -> false
+    # * A {TRUE_REGEXP} match -> true
+    # * A {FALSE_REGEXP} match -> false
+    #
+    # @param for the object to convert
+    # @return [Boolean, nil] the corresponding Boolean
+    # @raise [ArgumentError] if the value cannot be converted
+    def self.for(obj)
+      case obj
+      when nil then nil
+      when true then true
+      when false then false
+      when 1 then true
+      when 0 then false
+      when TRUE_REGEXP then true
+      when FALSE_REGEXP then false
+      else
+        raise ArgumentError.new("Value cannot be converted to boolean: '#{obj}'")
+      end
+    end
+  end
+end
 
 class TrueClass
-  include Boolean
+  include Jinx::Boolean
 end
 
 class FalseClass
-  include Boolean
-end
-
-class String
-  # Converts this string to a boolean as follows:
-  # * +true+, +t+, +yes+, +y+ or 1 => true
-  # * +false+, +f+, +no+, +n+ or 0 => false
-  #
-  # The comparison is case-insensitive.
-  #
-  # @return [Boolean] the boolean value
-  # @raise [ArgumentError] if this string does not match a supported representation
-  def to_boolean
-    case self
-    when /^(true|t|yes|y|1)$/i then true
-    when /^(false|f|no|n|0)$/i then false
-    else
-      raise ArgumentError.new("String value cannot be converted to boolean: '#{self}'")
-    end
-  end
-end
-
-class Integer
-  # Converts this integer to a boolean as follows:
-  # 1 => true
-  # 0 => false
-  #
-  # @return [Boolean] the boolean value
-  # @raise [ArgumentError] if this string does not match a supported representation
-  def to_boolean
-    case self
-    when 1 then true
-    when 0 then false
-    else
-      raise ArgumentError.new("Integer value cannot be converted to Boolean: #{self}")
-    end
-  end
+  include Jinx::Boolean
 end
