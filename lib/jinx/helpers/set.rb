e208f4a3f24e66231b3e04cf47728bf2a475c982
Fred Loney <loneyf@ohsu.edu>
2012-04-02 14:56:49 -0700

    Clarify comment.

--- lib/jinx/helpers/set.rb
+++ lib/jinx/helpers/set.rb
@@ -3,6 +3,6 @@ require 'set'
 class Set
   # The standard Set {#merge} is an anomaly among Ruby collections, since merge modifies the called Set in-place rather
   # than return a new Set containing the merged contents. Preserve this unfortunate behavior, but partially address
-  # the anomaly by adding the merge! alias for in-place merge.
+  # the anomaly by adding the merge! alias to make it clear that this is an in-place merge.
   alias :merge! :merge
 end
