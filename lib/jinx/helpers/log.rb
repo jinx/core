cd4051a21c3c8ff48a59b9bca285d472f07fb99a
Fred Loney <loneyf@ohsu.edu>
2012-05-11 10:29:16 -0700

    The :app option lets the logger find a writable app log file.

--- lib/jinx/helpers/log.rb
+++ lib/jinx/helpers/log.rb
@@ -3,6 +3,7 @@ require 'singleton'
 require 'ftools'
 require 'jinx/helpers/collections'
 require 'jinx/helpers/options'
+require 'jinx/helpers/inflector'
 
 # @param [String, IO, nil] dev the optional log file or device
 # @return [Jinx::MultilineLogger] the global logger
@@ -48,25 +49,30 @@ module Jinx
   class Log
     include Singleton
     
-    # Opens the log. The default log location is as follows:
-    # * +/var/log/caruby.log+ for Linux
-    # * +%LOCALAPPDATA%\caRuby\log\caruby.log+ for Windows
+    # Opens the log. The default log location is determined from the application name.
+    # The application name is the value of the +:app+ option, or +Jinx+ by default.
+    # For an application +MyApp+, the log location is determined as follows:
+    # * +/var/log/my_app.log+ for Linux
+    # * +%LOCALAPPDATA%\MyApp\log\MyApp.log+ for Windows
+    # * +./log/MyApp.log+ otherwise
     # The default file must be creatable or writable. If the device argument is not
     # provided and there is no suitable default log file, then logging is disabled.
     #
     # @param [String, IO, nil] dev the log file or device
     # @param [Hash, nil] opts the logger options
+    # @option opts [String] :app the application name
     # @option opts [Integer] :shift_age the number of log files retained in the rotation
     # @option opts [Integer] :shift_size the maximum size of each log file
     # @option opts [Boolean] :debug whether to include debug messages in the log file
     # @return [MultilineLogger] the global logger
     def open(dev=nil, opts=nil)
       raise RuntimeError.new("Log already open") if open?
-      if String === dev then File.makedirs(File.dirname(dev)) end
+      dev, opts = nil, dev if Hash === dev
+      dev ||= default_log_file(Options.get(:app, opts)) 
+      FileUtils.mkdir_p(File.dirname(dev)) if String === dev
       # default is 4-file rotation @ 16MB each
       shift_age = Options.get(:shift_age, opts, 4)
       shift_size = Options.get(:shift_size, opts, 16 * 1048576)
-      dev ||= default_log_file
       @logger = MultilineLogger.new(dev, shift_age, shift_size)
       @logger.level = Options.get(:debug, opts) ? Logger::DEBUG : Logger::INFO
       @logger.formatter = lambda do |severity, time, progname, msg|
@@ -110,30 +116,42 @@ module Jinx
     # The default log file.
     LINUX_LOG_DIR = '/var/log'
     
-    # Returns +caruby.log+ in the default log directory, determined as follows:
-    # * +/var/log+ for Linux
-    # * +%LOCALAPPDATA%//caRuby/log+ for Windows
+    # Returns the log file, as described in {#open}.
+    #
+    # If the standard Linux log location exists, then try that.
+    # Otherwise, try the conventional Windows app data location.
+    # If all else fails, use the working directory.
+    #
     # The file must be creatable or writable.
-    # 
-    # @return [String, nil] the file name if it is is creatable or writable, otherwise nil
-    def default_log_file
-      # If the standard Linux log location exists, then try that.
-      # Otherwise, try the conventional Windows app data location.
-      if File.exists?(LINUX_LOG_DIR) then
-        file = File.expand_path('caruby.log', LINUX_LOG_DIR)
-        file if File.exists?(file) ? File.writable?(file) : File.writable?(LINUX_LOG_DIR)
+    #
+    # @param [String, nil] app the application name (default +jinx+) 
+    # @return [String] the file name
+    def default_log_file(app=nil)
+      app ||= 'Jinx'
+      default_linux_log_file(app) || default_windows_log_file(app) || "log/#{app}.log"
+    end
+      
+    # @param [String] app the application name
+    # @return [String, nil] the default file name
+    def default_linux_log_file(app)
+      return unless File.exists?(LINUX_LOG_DIR)
+      base = app.underscore.gsub(' ', '_')
+      file = File.expand_path("#{base}.log", LINUX_LOG_DIR)
+      log = file if File.exists?(file) ? File.writable?(file) : File.writable?(LINUX_LOG_DIR)
+      log || '/dev/null'
+    end
+    
+    # @param [String] app the application name 
+    # @return [String, nil] the default file name
+    def default_windows_log_file(app)
+      # the conventional Windows app data location  
+      app_dir = ENV['LOCALAPPDATA'] || return
+      dir = app_dir + "/#{app}/log"
+      file = File.expand_path("#{app}.log", dir)
+      if File.exists?(file) ? File.writable?(file) : (File.directory?(dir) ? File.writable?(dir) : File.writable?(app_dir)) then
+        file
       else
-        # the conventional Windows app data location  
-        win_app_dir = ENV['LOCALAPPDATA'] || return
-        dir = win_app_dir + '/caRuby/log'
-        file = File.expand_path('caruby.log', dir)
-        if File.exists?(file) then
-          file if File.writable?(file)
-        elsif File.directory?(dir) then
-          file if File.writable?(dir)
-        elsif File.writable?(win_app_dir) then
-          file
-        end
+        'NUL'
       end
     end
   end
