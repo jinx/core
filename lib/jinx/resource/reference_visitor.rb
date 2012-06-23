82dfbb68193cc3b7fa3317041141d7eb74fba789
Fred Loney <loneyf@ohsu.edu>
2012-06-22 19:10:42 -0700

    Cosmetic changes.

--- lib/jinx/resource/reference_visitor.rb
+++ lib/jinx/resource/reference_visitor.rb
@@ -14,31 +14,31 @@ module Jinx
     # The required selector block given to this initializer determines which attributes to
     # visit. The references to visit next thus consist of the current domain object's selector
     # attributes' values. If the :filter option is set, then the given filter block is applied
-    # to the selected attribute references to restrict which domain objects will be visited.
+    # to each selected attribute reference to determine which domain objects will be visited.
     #
     # @param opts (see Visitor#initialize)
-    # @option opts [Proc] :filter an optional filter on the references to visit
+    # @option opts [Proc] :filter an optional filter on the reference to visit
     # @yield [obj] returns the {AttributeEnumerator} of attributes to visit next from the
     #   current domain object
     # @yieldparam [Resource] obj the current domain object
     def initialize(opts=nil, &selector)
       raise ArgumentError.new("Reference visitor missing domain reference selector") unless block_given?
       # the property selector
-      @flt_sel = selector
+      @selector = selector
       # the reference filter
-      flt = Options.get(:filter, opts)
+      @filter = Options.get(:filter, opts)
       # Initialize the Visitor with a reference enumerator which selects the reference
       # attributes and applies the optional filter if necessary.
       @ref_enums = {}
-      super do |ref|
+      super do |obj|
         # the reference property filter
-        ras = attributes_to_visit(ref)
+        ras = attributes_to_visit(obj)
         if ras then
-          logger.debug { "#{qp} visiting #{ref} attributes #{ras.pp_s(:single_line)}..." } if @verbose
+          logger.debug { "#{qp} visiting #{obj} attributes #{ras.pp_s(:single_line)}..." } if @verbose
           # an enumerator on the reference properties
-          enum = ReferenceEnumerator.new(ref, ras.properties)
+          enum = ReferenceEnumerator.new(obj, ras.properties)
           # If there is a reference filter, then apply it to the enum references.
-          flt ? enum.filter(&flt) : enum
+          @filter ? enum.filter(&@filter) : enum
         end
       end
     end
@@ -48,7 +48,7 @@ module Jinx
     # @param [Resource] obj the visiting object
     # @return [Propertied::Filter] the attributes to visit
     def attributes_to_visit(obj)
-      @flt_sel.call(obj)
+      @selector.call(obj)
     end
   end
 end
