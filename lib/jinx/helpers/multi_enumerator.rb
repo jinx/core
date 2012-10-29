ea8a797de30af95747a095f13c898b01481ab149
Fred Loney <loneyf@ohsu.edu>
2012-10-29 11:16:43 -0700

    Support MultiEnumerator appender.

--- lib/jinx/helpers/multi_enumerator.rb
+++ lib/jinx/helpers/multi_enumerator.rb
@@ -1,3 +1,5 @@
+require 'jinx/helpers/collection'
+
 module Jinx
   # A MultiEnumerator iterates over several Enumerators in sequence. Unlike Array#+, MultiEnumerator reflects changes to the
   # underlying enumerators.
@@ -12,20 +14,34 @@ module Jinx
     include Collection
 
     # @return [<Enumerable>] the enumerated collections
-    attr_reader :components
+    attr_reader :components, :appender
 
     # Initializes a new {MultiEnumerator} on the given components.
     #
     # @param [<Enumerable>] the component enumerators to compose
-    def initialize(*enums)
+    # @yield [item] the optional appender block
+    # @yieldparam item the item to append
+    def initialize(*enums, &appender)
       super()
       @components = enums
       @components.compact!
+      @appender = appender
     end
 
-    # Iterates over each of this MultiEnumerator's Enumerators in sequence.
+    # Iterates over each of this MultiEnumerator's enumerators in sequence.
     def each
       @components.each { |enum| enum.each { |item| yield item  } }
+    end
+    
+    # @param item the item to append
+    # @raise [NoSuchMethodError] if this {MultiEnumerator} does not have an appender
+    def <<(item)
+      @appender ? @appender << item : super
+    end
+      
+    # Returns the union of the results of calling the given method symbol on each component.
+    def method_missing(symbol, *args)
+      self.class.new(@components.map { |enum|enum.send(symbol, *args) })
     end
   end
 end
